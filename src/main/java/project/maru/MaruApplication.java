3aa5e8ad24eaee4fd24bf505ac719ed88bb18938
bm150 <jbm20dev@gmail.com>
2024-07-22 16:46:02 +0900

    test : debug url

--- src/main/java/project/maru/MaruApplication.java
+++ src/main/java/project/maru/MaruApplication.java
@@ -1,13 +1,29 @@
 package project.maru;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MaruApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MaruApplication.class, args);
-	}
+  @Value("${spring.datasource.url}")
+  private String datasourceURL;
 
+  @Value("${server.port}")
+  private String serverPort;
+
+  public static void main(String[] args) {
+    SpringApplication.run(MaruApplication.class, args);
+  }
+
+  @PostConstruct
+  public void logEnvVariables() {
+    log.debug("datasourceURL: {}", datasourceURL);
+    log.debug("serverPort: {}", serverPort);
+
+  }
 }
